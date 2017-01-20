e3a6a249cc5e424410f0862b68453e47d5a4e446
Matan Sokolovsky <matanso@gmail.com>
2017-01-20 17:37:37 +0200

    refactor

--- src/FileList/index.js
+++ src/FileList/index.js
@@ -7,32 +7,31 @@ import File from '../File/index';
 import Directory from '../Directory/index';
 
 function compare(file1, file2) {
-    if(file1.type !== file2.type) return file1.type === 'folder' ? -1 : 1;
-    return file1.name < file2.name ? -1: 1;
+    if (file1.type !== file2.type) return file1.type === 'folder' ? -1 : 1;
+    return file1.name < file2.name ? -1 : 1;
 }
 
 
 class FileList extends React.Component {
     render() {
-        if(!Array.isArray(this.props.files)) return(<div></div>);
-        let clickHandler = this.props.onClick;
+        if (!Array.isArray(this.props.files)) return (<div></div>);
         this.props.files.sort(compare);
         return (
             <div>
-                {this.props.files.map(create)}
+                {this.props.files.map(create, this)}
             </div>
         );
 
 
         function create(file, index) {
-            console.log(index);
             switch (file.type) {
                 case 'folder':
-                    return <Directory name={file.name} onClick={() => clickHandler(index)}/>;
+                    return <Directory key={index} name={file.name} onClick={() => this.props.onClick(index)}/>;
                 case 'file':
-                    return <File name={file.name}/>;
+                    return <File key={index} name={file.name}/>;
+                default:
+                    throw new Error(`Unknown object type ${file.type}`);
             }
-            throw new Error(`Unknown object type ${file.type}`);
         }
     }
 }
